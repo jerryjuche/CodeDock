d8147dff10a021b0354373ed26931b8dbb8f08d2
juchechu <maxjerry242@gmail.com>
2026-03-26 18:07:48 +0100

    ..

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.25.0
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.1
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.12.0
 	golang.org/x/crypto v0.49.0
 )
--- main.go
+++ main.go
@@ -1,24 +1,19 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
-	"net/http"
-	"os"
 
-	"github.com/jerryjuche/CodeDock/internal/handlers"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 
-	if err := godotenv.Load(); err != nil{
+	if err := godotenv.Load(); err != nil {
 		log.Println("no .env file found, reading from system environment")
 		return
 	}
 
 	db, err := connectDB()
-	
+
 }
