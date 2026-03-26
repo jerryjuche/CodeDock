95725dfd2d662f4c1662a16712c235f02b726034
juchechu <maxjerry242@gmail.com>
2026-03-26 13:15:47 +0100

    handled user auth and login verification

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jerryjuche/CodeDock
 
 go 1.25.0
 
-require github.com/golang-jwt/jwt/v5 v5.3.1
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.1
+	golang.org/x/crypto v0.49.0
+)
--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,1 +1,114 @@
 package handlers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	"github.com/jerryjuche/CodeDock/internal/auth"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthHandler struct {
+	DB *sql.DB
+}
+
+type registerRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type authResponse struct {
+	Token string `json:"token"`
+	Email string `json:"email"`
+}
+
+func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Response) {
+	var req registerRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// verify email and password before writing to the db
+	if req.Email == "" || req.Password == "" {
+		http.Error(w, "email and password are required", http.StatusBadRequest)
+	}
+
+	// hash passwords before storing
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), 12)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	var UserID string
+
+	err = h.DB.QueryRow(`INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id`, req.Email, hashedPassword).Scan(&UserID)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusBadRequest)
+	}
+
+	token, err := auth.GenerateToken(UserID, req.Email)
+	if err != nil {
+		http.Error(w, "could not generate token", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(authResponse{
+		Token: token,
+		Email: req.Email,
+	})
+
+}
+
+func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Response) {
+	var req registerRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	//check for empty mail and password
+	if req.Email == "" || req.Password == "" {
+		http.Error(w, "email and password are required", http.StatusBadRequest)
+		return
+	}
+
+	// fetch users from db by email
+	var userID, passwordHash string
+
+	err := h.DB.QueryRow(`SELECT id, password_hash FROM users WHERE email = $1`, req.Email).Scan(&userID, &passwordHash)
+
+	if err == sql.ErrNoRows {
+		http.Error(w, "invalid credentials", http.StatusUnauthorized)
+	}
+
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+	}
+
+	// compare hashed password
+	if err = bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(req.Password)); err != nil {
+		http.Error(w, "invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	// generate token
+	token, err := auth.GenerateToken(userID, req.Email)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(authResponse{
+		Token: token,
+		Email: req.Email,
+	})
+}
